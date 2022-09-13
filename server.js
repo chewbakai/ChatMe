a48a5782f33bd2c9ab0a24f2da836ae13606ebae
jbm24 <58428331+jbm24@users.noreply.github.com>
2022-09-13 23:36:31 +0800

    changed bot name

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-const botName = "ChatCord Bot";
+const botName = "ChatMe Bot";
 
 (async () => {
   pubClient = createClient({ url: "redis://127.0.0.1:6379" });
